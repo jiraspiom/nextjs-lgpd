6ddcaedbf6bbd1fa7d7a4a2361f7de6ccd528b3c
1gilmar <1gilmar@gmail.com>
2026-05-16 08:12:11 -0300

    ok

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Footer(){
+    return(
+        <footer style={{ padding: '1rem', textAlign: 'center', borderTop: '1px solid #ccc' }}>
+      <p>&copy; {new Date().getFullYear()} Seu Site. Todos os direitos reservados.</p>
+      <div style={{ marginTop: '0.5rem' }}>
+        <Link href="/politica-privacidade" style={{ textDecoration: 'underline', color: '#0070f3' }}>
+          Política de Privacidade
+        </Link>
+      </div>
+    </footer>
+    )
+}
