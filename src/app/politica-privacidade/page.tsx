0d47967c38abcc101608811cf556abad81d6d613
1gilmar <1gilmar@gmail.com>
2026-05-16 08:01:05 -0300

    politica

---
+++ src/app/politica-privacidade/page.tsx
@@ -0,0 +1,7 @@
+export default function PoliticaPrivacidade() {
+    return <main style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
+      <h1>Política de Privacidade</h1>
+      <p>Sua privacidade é importante para nós. É política do nosso site respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar...</p>
+      {/* Adicione o restante do seu texto de privacidade aqui */}
+    </main>
+}
